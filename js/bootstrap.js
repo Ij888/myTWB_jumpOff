1256ecf913422e9527646808f7e1d0f939af76ad
Ijem <ij.ofili@gmail.com>
2016-12-28 16:37:14 +0000

    added entry

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -47,7 +47,7 @@ if (typeof jQuery === 'undefined') {
       }
     }
 
-    return false // explicit for ie8 (  ._.)
+    return false // explicit for ie8 ( ._.)
   }
 
   // http://blog.alexmaccaw.com/css-transitions
